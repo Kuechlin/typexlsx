ce25704e33802810d9ea4498062484e4fa701f7a
Y. Küchlin <y.kuechlin@gmx.de>
2021-12-21 20:10:01 +0100

    vite docs config fix

--- docs.vite.config.ts
+++ docs.vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/typexlsx/',
     build: {
         outDir: path.resolve(__dirname, 'build'),
-        base: '/typexlsx/',
     },
 });
